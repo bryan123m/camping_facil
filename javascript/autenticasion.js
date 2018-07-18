5849cc067a8578fc40523ca93cfc55e42cd4d6de
bryan123m <bryanmzr183.com>
2018-07-18 18:32:12 -0500

    comienzo

--- javascript/autenticasion.js
+++ javascript/autenticasion.js
@@ -1,61 +1,58 @@
 jQuery(document).ready(function($){
-$('.login').on(
-'submit',
-function(e){
-	e.preventDefault();
+	$('.login').on(
+		'submit',
+		function(e){
+			e.preventDefault();
 
-	var email = $('.login .email').val();
-	var password = $('.login .password').val();
+			var email = $('.login .email').val();
+			var password = $('.login .password').val();
 
-	if(!email) {
-		alert('Debe ingresar un correo');
-	}else if(!password) {
-		alert('Debe ingresar una contraseña');
-	}else {
-	
-	firebase
-	.auth().signInWithEmailAndPassword(email, password)
-    .then(function() {
-  		alert('Bienvenido');
-	})
-  	.catch(function(error) {
-  		alert('No eres Bienvenido');
+			if(!email) {
+				alert('Debe ingresar un correo');
+			}else if(!password) {
+				alert('Debe ingresar una contraseña');
+			}else {
+
+				firebase
+				.auth().signInWithEmailAndPassword(email, password)
+				.then(function() {
+					alert('Bienvenido');
+					
+				})
+				.catch(function(error) {
+					alert('No eres Bienvenido');
 
 
-  	
-  		
+
+
+				});
+			}
+		}
+		
+		);
+	$('.registro').on(
+		'submit',
+		function(e){
+			e.preventDefault();
+
+			var email = $('.registro .email').val();
+			var password = $('.registro .password').val();
+
+			if(!email) {
+				alert('Debe ingresar un correo');
+			}else if(!password) {
+				alert('Debe ingresar una contraseña');
+			}else {
+				firebase.auth().createUserWithEmailAndPassword(email, password)
+				.then(function() {
+					alert('Bienvenido');
+					console.log(email,password);
+				})
+				.catch(function(error) {
+					alert('No eres Bienvenido');
 				});
 			};
-		  }
+		}
 		
-	);
-});
-jQuery(document).ready(function($){
-$('.login').on(
-'submit',
-function(e){
-	e.preventDefault();
-
-	var email = $('.login .email').val();
-	var password = $('.login .password').val();
-
-	if(!email) {
-		alert('Debe ingresar un correo');
-	}else if(!password) {
-		alert('Debe ingresar una contraseña');
-	}else {
-		firebase
-	.auth().createUserWithEmailAndPassword(email, password)
-    .then(function() {
-  		alert('Bienvenido');
-	})
-  	.catch(function(error) {
-  		alert('No eres Bienvenido');
-
-	
-				});
-			};
-		  }
-		
-	);
+		);
 });
--- javascript/firebase.js
+++ javascript/firebase.js
@@ -1,10 +1,10 @@
-  var config = {
-    apiKey: "AIzaSyDhTqG9TSBB6xJTEq35gj3PST7Y4xrFQgU",
-    authDomain: "chat-2d6e1.firebaseapp.com",
-    databaseURL: "https://chat-2d6e1.firebaseio.com",
-    projectId: "chat-2d6e1",
-    storageBucket: "chat-2d6e1.appspot.com",
-    messagingSenderId: "627360866822"
-  };
-  firebase.initializeApp(config);
-  const database = firebase.database();
+var config = {
+  apiKey: "AIzaSyByY6duXVpOrmEg9arg8RPWdW4wXkAPmpw",
+  authDomain: "camping-facil-9e5df.firebaseapp.com",
+  databaseURL: "https://camping-facil-9e5df.firebaseio.com",
+  projectId: "camping-facil-9e5df",
+  storageBucket: "",
+  messagingSenderId: "850135452594"
+};
+firebase.initializeApp(config);
+const database = firebase.database();
